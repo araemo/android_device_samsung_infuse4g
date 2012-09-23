85de13bf729e98d59e346c7e8fc49689a1f8b756
jt1134 <jt1134@gmail.com>
2012-09-22 23:15:39 -0500

    use logical volumes for system and data, bring back datadata
    * use lvm to split eMMC into 2 logical partitions
    * use entire available space on OneNAND for /datadata
    
    based on information from: http://bit.ly/OMny8Z
    
    WARNING: WILL WIPE DATA
    
    Modified for Infuse4g by Araemo
    
    Change-Id: I3caeec096a6798afafaf03b2eb72c20580272682
    
    Conflicts:
    
    	fstab.aries
    	infuse4g.mk
    	init.aries.rc
    	recovery.fstab
    	updater.sh

--- infuse4g.mk
+++ infuse4g.mk
@@ -53,7 +53,9 @@ PRODUCT_COPY_FILES += \
 	device/samsung/infuse4g/init.aries.usb.rc:root/init.aries.usb.rc \
 	device/samsung/infuse4g/init.aries.usb.rc:recovery/root/usb.rc \
 	device/samsung/infuse4g/ueventd.aries.rc:root/ueventd.aries.rc \
-	device/samsung/infuse4g/lpm.rc:root/lpm.rc
+	device/samsung/infuse4g/fstab.aries:root/fstab.aries \
+	device/samsung/infuse4g/lpm.rc:root/lpm.rc \
+	device/samsung/aries-common/setupdatadata.sh:root/sbin/setupdatadata.sh
 
 # Prebuilt kl keymaps
 PRODUCT_COPY_FILES += \
